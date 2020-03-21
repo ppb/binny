97e7062543799cb3b862bfe01c18e59d34462494
Jamie Bliss <jamie@ivyleav.es>
2020-03-21 14:52:26 -0400

    app.py: Initial implementation of triggering self-deployment

--- binny/app.py
+++ binny/app.py
@@ -12,6 +12,7 @@ from quart import Quart, request
 import quart_github_webhook
 
 from .globals import ghapp
+from .checks import get_check_suites
 
 log = logging.getLogger(__name__)
 
@@ -44,11 +45,49 @@ GitHub: {'ok' if gh_ok else 'err'}
 """, 200 if gh_ok else 500)
 
 
-@webhook.hook('check_run')
-async def check_run(payload):
+@webhook.hook('check_suite')
+async def check_suite(payload):
     """
-    https://developer.github.com/v3/activity/events/types/#checkrunevent
+    https://developer.github.com/v3/activity/events/types/#checksuiteevent
     """
+    # Trigger deployment if:
+    # * The binny repo
+    # * On main branch
+    # * All checks have passed
+    repo = payload['repository']
+    suite = payload['check_suite']
+    # Sets up authentication so act as the installation
+    async with ghapp.for_installation(payload['installation']['id'], repo_id=repo['id']):
+        if repo['full_name'] != 'ppb/binny':
+            # Not the repo
+            return
+
+        if suite['head_branch'] != repo['default_branch']:
+            # Not the branch
+            return
+
+        # UGH, doesn't actually send us the node_id of the commit, just the git sha
+        commit_sha = suite['head_commit']['id']
+        resp = await get_check_suites(repo=repo['node_id'], commit=commit_sha)
+        assert not resp.errors, resp.errors
+        suites = resp.data['node']['object']['checkSuites']['nodes']
+        has_succeeded = all(
+            s['status'] == 'COMPLETED' and s['conclusion'] == 'SUCCESS'
+            for s in suites
+        )
+        if not has_succeeded:
+            # Not all checks passed
+            return
+
+        # OK, now we can deploy
+        log = open('/tmp/binny-deploy.log')
+        subprocess.Popen(
+            [sys.executable, '-m', 'binny.deploy'],
+            stdin=subprocess.DEVNULL,
+            stdout=log,
+            stderr=log,
+        )
+        # Allow to run in the background
 
 
 async def entrypoint(scope, receive, send):
