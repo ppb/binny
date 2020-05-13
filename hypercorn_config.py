7908c0ffe207a47b041665e17c389f7145345586
Jamie Bliss <jamie@ivyleav.es>
2020-05-13 13:23:09 -0400

    Tweak error log format

--- hypercorn_config.py
+++ hypercorn_config.py
@@ -31,7 +31,7 @@ logconfig_dict = {
     },
     "formatters": {
         "generic": {
-            "format": "%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
+            "format": "[%(name)s]%(levelname)s: %(message)s",
             "datefmt": "[%Y-%m-%d %H:%M:%S]",
             "class": "logging.Formatter",
         }
